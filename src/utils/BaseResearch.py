f3ef88be3da61eb441efd99cac7873fd263f4c36
sharadmishra28 <53832290+sharadmishra28@users.noreply.github.com>
2022-01-31 00:13:27 +0530

    update code for raw file location from yaml

--- src/utils/BaseResearch.py
+++ src/utils/BaseResearch.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from docx import Document
 import argparse
+import os
 from src.utils.logg import App_Logger
 from src.utils.common import read_yaml
 from src.utils.common import create_dir
@@ -10,26 +11,28 @@ class Rawdata:
         self.config_content = config_content
         __artifact = config_content['artifacts']
         __dir = os.path.join(__artifact['ARTIFACTS_DIR'],__artifact['LOG_DIR'])
-        file_path = f'{_dir}/Rawdata.txt'
+
+        file_path = os.path.join(__dir,'Rawdata.txt')
         self.file_object = open(file_path, 'a+')
         self.log_writer = App_Logger()
         self.log_writer.log(self.file_object, "file logger started ")
 
-    def read_docx(self, path, num: int = 0):
+    def read_docx(self, num: int = 0):
         """
         This method will read the MS document for table content.
         return in dataframe format
 
         Note: it will consider first row as header of table
 
-        :param path: location of document file
+
         :param num: #no. of table in the document. by default it will take the first table
         :return: Return the table content in pandas dataframe format
         """
         try:
             __artifact_dir = self.config_content['artifacts']['ARTIFACTS_DIR']
-            __path = self.config_content['artifacts']['RAW_DATA']
-            path = os.path.join(__artifact_dir,__path)
+            __raw_path = self.config_content['artifacts']['RAW_DATA']
+            __path = self.config_content['artifacts']['RAW_FILE']
+            path = os.path.join(__artifact_dir,__raw_path,__path)
             self.log_writer.log(self.file_object,"Starting extraction of dataset")
             #creating object for document
             __document = Document(path)
@@ -75,8 +78,8 @@ if __name__ == '__main__':
         config_content = read_yaml(config_path)
         # rw_path = content['artifacts']['RAW_DATA']
         rw=Rawdata(config_content)
-        rw.read_docx(rw_path)
-        # main(config_path=parsed_args.config, params_path=parsed_args.params)
+        rw.read_docx()
+
         Gen_log.Gen_log(f">>>>> Raw_data is extracted from DOCX to CSV format<<<<<\n")
     except Exception as e:
         Gen_log.Gen_log(f" exception :  {e}")
