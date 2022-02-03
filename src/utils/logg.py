b00bc86a1d21115c3755d56e7883257acfd03c49
sharadmishra28 <53832290+sharadmishra28@users.noreply.github.com>
2022-02-03 12:19:01 +0530

    logg.py under-progress

--- src/utils/logg.py
+++ src/utils/logg.py
@@ -2,6 +2,13 @@ from datetime import datetime
 import os
 from src.utils.common import create_dir
 
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'),
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
 class App_Logger:
     def __init__(self):
 
@@ -15,10 +22,10 @@ class App_Logger:
         file_object.write(
             str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message +"\n")
 
-    def Gen_log(self, log_message):
-        __file = open("artifacts/log/Gen.txt", 'a+')
-        self.now = datetime.now()
-        self.date = self.now.date()
-        self.current_time = self.now.strftime("%H:%M:%S")
-        __file.write(
-            str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + "\n")
+    # def Gen_log(self, log_message):
+    #     __file = open("artifacts/log/Gen.txt", 'a+')
+    #     self.now = datetime.now()
+    #     self.date = self.now.date()
+    #     self.current_time = self.now.strftime("%H:%M:%S")
+    #     __file.write(
+    #         str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + "\n")
