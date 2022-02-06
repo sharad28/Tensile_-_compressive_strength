93374f1ed8579d64b1bd238408bcb17134d61854
sharadmishra28 <53832290+sharadmishra28@users.noreply.github.com>
2022-02-06 20:01:41 +0530

    test script updated

--- tests/test_script.py
+++ tests/test_script.py
@@ -16,15 +16,6 @@ class TestToPerform(unittest.TestCase):
         print(response)
         self.assertEqual(response.status_code, 200)
 
-    def test_page1(self):
-        response = self.app.get('/test', follow_redirects=True)
-        print(response)
-        print("*******************"
-              "************************"
-              "*********************")
-
-        print(response.json)
-        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
